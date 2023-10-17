a0ab22d9a5db6127d7f3408ac4a56cd6643d7ac5
unknown <atikrahad1@gmail.com>
2023-10-17 22:11:53 +0600

    adding navber and home router

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navber from "../SharedComponents/Navber";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navber></Navber>
+        </div>
+    );
+};
+
+export default Home;
