5361d1e7c59aa9d7ec48043aa7b68d59dbf9d00a
unknown <atikrahad1@gmail.com>
2023-10-19 19:43:58 +0600

    adding brand sector in homepage

---
+++ src/Components/Brand.jsx
@@ -0,0 +1,25 @@
+const Brand = ({ data }) => {
+
+    const {img, brand} = data;
+  return (
+    <div>
+      <div className="card h-64  shadow-xl image-full">
+        <figure>
+          <img className="w-full"
+            src={img}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{brand}</h2>
+          <p></p>
+          <div className="card-actions justify-end">
+            <button className="btn border-none text-white font-semibold bg-green-600 hover:bg-sky-600">See {brand} products</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Brand;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import Header from "../Components/Header";
+import Brand from "../Components/Brand";
 
 
 
 const Home = () => {
+
+    const [branddata, setBranddata] = useState([])
+    useEffect(()=>{
+        fetch('brand.json')
+        .then(res => res.json())
+        .then(data=> setBranddata(data))
+    },[])
     return (
         <div>
             <Header></Header>
+            <h1 className="text-3xl font-bold text-center pt-20">Explore our brands products</h1>
+            <div className=" py-10 gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[95%] md:w-[80%] mx-auto">
+                {
+                    branddata.map(data => <Brand key={data.id} data={data}></Brand>)
+                }
+            </div>
         </div>
     );
 };
